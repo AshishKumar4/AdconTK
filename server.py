d2db17b50f36e3a023fdcde2e000eb5989346dbf
root <ashishkmr472@gmail.com>
2018-09-22 15:10:18 +0530

    Access over FTP Supported

---
+++ server.py
@@ -0,0 +1,15 @@
+# Author : WhiteOracle
+
+
+from cmdFile import *
+
+
+cc = cmdFile('ttg')
+while True:
+    s = input(">>")
+    cc.putCommand(s)
+    p = cc.getLastOutput()
+    while len(p) == 0:
+        p = cc.getLastOutput()
+    print("\n")
+    print(str(p.decode("utf-8")))
